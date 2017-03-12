850efd9f650ba079bc92791f5bcd4f57e5b2ad67
rniem379349 <robert.niemiec95@wp.pl>
2017-03-12 13:58:50 +0100

    Add files via upload

---
+++ coke_piano.ino
@@ -0,0 +1,89 @@
+#include <CapacitiveSensor.h>
+
+// capacitive sensor initialization
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2); // 2 is sensor pin 
+CapacitiveSensor   cs_4_3 = CapacitiveSensor(4,3);
+//CapacitiveSensor   cs_4_5 = CapacitiveSensor(4,5);
+//CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);
+//CapacitiveSensor   cs_4_7 = CapacitiveSensor(4,7);
+//CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);
+//CapacitiveSensor   cs_4_9 = CapacitiveSensor(4,9);
+//CapacitiveSensor   cs_4_10 = CapacitiveSensor(4,10);
+//CapacitiveSensor   cs_4_11 = CapacitiveSensor(4,11);
+//CapacitiveSensor   cs_4_12 = CapacitiveSensor(4,12);
+//CapacitiveSensor   cs_4_13 = CapacitiveSensor(4,13);
+//CapacitiveSensor   cs_4_14 = CapacitiveSensor(4,14);
+//CapacitiveSensor   cs_4_15 = CapacitiveSensor(4,15);
+//CapacitiveSensor   cs_4_16 = CapacitiveSensor(4,16);
+//CapacitiveSensor   cs_4_17 = CapacitiveSensor(4,17);
+//CapacitiveSensor   cs_4_18 = CapacitiveSensor(4,18);
+
+void setup() 
+{
+  // set up serial communication and calibrate sensors
+  Serial.begin(19200);
+   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);
+   cs_4_3.set_CS_AutocaL_Millis(0xFFFFFFFF);  
+//   cs_4_5.set_CS_AutocaL_Millis(0xFFFFFFFF);
+//   cs_4_6.set_CS_AutocaL_Millis(0xFFFFFFFF);
+//   cs_4_7.set_CS_AutocaL_Millis(0xFFFFFFFF);
+//   cs_4_8.set_CS_AutocaL_Millis(0xFFFFFFFF); 
+//   cs_4_9.set_CS_AutocaL_Millis(0xFFFFFFFF);
+//   cs_4_10.set_CS_AutocaL_Millis(0xFFFFFFFF); 
+//   cs_4_11.set_CS_AutocaL_Millis(0xFFFFFFFF);
+//   cs_4_12.set_CS_AutocaL_Millis(0xFFFFFFFF); 
+//   cs_4_13.set_CS_AutocaL_Millis(0xFFFFFFFF);
+//   cs_4_14.set_CS_AutocaL_Millis(0xFFFFFFFF); 
+//   cs_4_15.set_CS_AutocaL_Millis(0xFFFFFFFF);
+//   cs_4_16.set_CS_AutocaL_Millis(0xFFFFFFFF); 
+//   cs_4_17.set_CS_AutocaL_Millis(0xFFFFFFFF);
+//   cs_4_18.set_CS_AutocaL_Millis(0xFFFFFFFF); 
+}
+
+void loop() {
+
+  if(Serial.available() >= 0) 
+  {
+    // Measure the sensors
+    long total_2 = cs_4_2.capacitiveSensor(30);
+//    Serial.println(total_2);
+    
+    long total_3 = cs_4_3.capacitiveSensor(30);
+//    Serial.println(total_3);
+//    delay(100);
+//    long total_5 = cs_4_5.capacitiveSensor(30);
+//    long total_6 = cs_4_6.capacitiveSensor(30);
+//    long total_7 = cs_4_7.capacitiveSensor(30);
+//    long total_8 = cs_4_8.capacitiveSensor(30);
+//    long total_9 = cs_4_9.capacitiveSensor(30);
+//    long total_10 = cs_4_10.capacitiveSensor(30);
+//    long total_11 = cs_4_11.capacitiveSensor(30);
+//    long total_12 = cs_4_12.capacitiveSensor(30);
+//    long total_13 = cs_4_13.capacitiveSensor(30);
+//    long total_14 = cs_4_14.capacitiveSensor(30);
+//    long total_15 = cs_4_15.capacitiveSensor(30);
+//    long total_16 = cs_4_16.capacitiveSensor(30);
+//    long total_17 = cs_4_17.capacitiveSensor(30);
+//    long total_18 = cs_4_18.capacitiveSensor(30);
+  
+  
+  //check if a sensor has been touched and send the corresponding index through serial to the .py script
+
+  if (total_2 > 500){Serial.println(0); delay(250);}
+  if (total_3 > 500){Serial.println(1); delay(250);}
+//  if (total_5 > 60){Serial.write("s\n"); delay(10);}
+//  if (total_6 > 60){Serial.write("e\n"); delay(10);}
+//  if (total_7 > 60){Serial.write("d\n"); delay(10);}
+//  if (total_8 > 60){Serial.write("f\n"); delay(10);}
+//  if (total_9 > 60){Serial.write("t\n"); delay(10);}
+//  if (total_10 > 60){Serial.write("g\n"); delay(10);}
+//  if (total_11 > 60){Serial.write("y\n"); delay(10);}
+//  if (total_12 > 60){Serial.write("h\n"); delay(10);}
+//  if (total_13 > 60){Serial.write("u\n"); delay(10);}
+//  if (total_14 > 60){Serial.write("j\n"); delay(10);}
+//  if (total_15 > 60){Serial.write("k\n"); delay(10);}
+//  if (total_16 > 60){Serial.write("o\n"); delay(10);}
+//  if (total_17 > 60){Serial.write("l\n"); delay(10);}
+//  if (total_18 > 60){Serial.write("p\n"); delay(10);}
+  }
+}
