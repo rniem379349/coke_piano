e762818f138824ccbe2717465612037b1957681b
rniem379349 <robert.niemiec95@wp.pl>
2017-03-12 19:21:28 +0100

    implement stack for simultaneous note handling (test)

--- coke_piano.py
+++ coke_piano.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1250 -*-
 # Coke piano .py script
 # Date: 12.03.2017
 # Creators:
@@ -9,10 +10,54 @@ import serial
 import mido
 import sys
 
+
+class Node:
+    def __init__(self, val = None, next_node = None, prev_node = None):
+        self.val = val
+        self.prev_node = prev_node
+        self.next_node = next_node
+
+
+class Stack:
+    def __init__(self):
+        self.len = 0
+        self.tail = None
+
+
+    def is_empty(self):
+        return self.len == 0
+
+
+    def push(self, val):
+        prev_tail = self.tail
+        self.tail = Node(val)
+
+        if not self.is_empty():
+            self.tail.prev_node = prev_tail
+            prev_tail.next_node = self.tail
+        self.len += 1
+
+
+    def pop(self):
+        if self.is_empty:
+            pass
+        else:
+            popped_val = self.tail.val
+            self.tail = self.tail.prev_node
+            self.len -= 1
+
+    def top(self):
+        if self.is_empty():
+            raise ValueError("The Stack is empty")
+        else:
+            return self.tail.val
+
+
 if __name__ == "__main__":
     # initialize mido lib for output
     out = mido.open_output()
-
+    # initialize stack for input
+    stack = Stack()
     # initialize serial connection to the arduino port
     try:
         serial_port = "com{}".format(sys.argv[1])
@@ -23,8 +68,11 @@ if __name__ == "__main__":
             val = ser.readline()
             print "val: ", val
             val = int(val)
+            stack.push(val)
+            stack_note = stack.top()
 
             try:
-                out.send(mido.Message('note_on', note=val+60, velocity=120, channel=0))
-            except TypeError:
+                out.send(mido.Message('note_on', note=stack_note+60, velocity=120, channel=0))
+                stack.pop()
+            except TypeError or ValueError:
                 pass
